3a47ab403be735c96af162f5095e21378afee1df
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-05-19 07:57:31 +0600

    site complate

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,6 +4,51 @@ const Home = () => {
   return (
     <div>
       <h1>This Is home section</h1>
+
+      <div class="carousel carousel-end rounded-box">
+        <div class="carousel-item">
+          <img
+            src="https://api.lorem.space/image/drink?w=400&h=300&hash=8B7BCDC2"
+            alt="Drink"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="https://api.lorem.space/image/drink?w=400&h=300&hash=500B67FB"
+            alt="Drink"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="https://api.lorem.space/image/drink?w=400&h=300&hash=A89D0DE6"
+            alt="Drink"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="https://api.lorem.space/image/drink?w=400&h=300&hash=225E6693"
+            alt="Drink"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="https://api.lorem.space/image/drink?w=400&h=300&hash=9D9539E7"
+            alt="Drink"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="https://api.lorem.space/image/drink?w=400&h=300&hash=BDC01094"
+            alt="Drink"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="https://api.lorem.space/image/drink?w=400&h=300&hash=7F5AE56A"
+            alt="Drink"
+          />
+        </div>
+      </div>
     </div>
   );
 };
